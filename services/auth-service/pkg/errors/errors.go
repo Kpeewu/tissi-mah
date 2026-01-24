b83e1b1db79f39ee5b5c7f1ee4304faad7af02a4
toureydaou <ttoureydaou@gmail.com>
2026-01-24 15:44:27 +0100

    feat(errors): add custom errors for auth-service

---
+++ services/auth-service/pkg/errors/errors.go
@@ -0,0 +1,12 @@
+package errors
+
+import "errors"
+
+var (
+	ErrorInternalServer          = errors.New("ErrorInternalServer")
+	ErrorUserNotFound            = errors.New("ErrorUserNotFound")
+	ErrorEmailNotAvailable       = errors.New("ErrorEmailNotAvailable")
+	ErrorPhoneNumberNotAvailable = errors.New("ErrorPhoneNumberNotAvailable")
+	ErrorDataRetrievalFailed     = errors.New("ErrorDataRetrievalFailed")
+	ErrorCantDeleteAccount       = errors.New("ErrorCantDeleteAccount")
+)
