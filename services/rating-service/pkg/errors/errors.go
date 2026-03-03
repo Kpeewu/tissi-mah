ff5342f8cfb67063cd89810bb72dbe65b5f8c4ad
Samrou Teouri <teourisamrou@gmail.com>
2026-03-03 23:39:57 +0100

    feat(rating-service): add sentinel error types

---
+++ services/rating-service/pkg/errors/errors.go
@@ -0,0 +1,14 @@
+package errors
+
+import "errors"
+
+var (
+	ErrorInternalServer      = errors.New("ErrorInternalServer")
+	ErrorRatingNotFound      = errors.New("ErrorRatingNotFound")
+	ErrorRatingAlreadyExists = errors.New("ErrorRatingAlreadyExists")
+	ErrorInvalidStars        = errors.New("ErrorInvalidStars")
+	ErrorSelfRating          = errors.New("ErrorSelfRating")
+	ErrorDataRetrievalFailed = errors.New("ErrorDataRetrievalFailed")
+	ErrorUnauthorizedAction  = errors.New("ErrorUnauthorizedAction")
+	ErrorCantDeleteRating    = errors.New("ErrorCantDeleteRating")
+)
