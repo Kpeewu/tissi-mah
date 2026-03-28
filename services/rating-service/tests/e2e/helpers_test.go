480eb639841729ed105609f25a67b9a9942cd0af
Samrou Teouri <teourisamrou@gmail.com>
2026-03-28 04:10:49 +0100

    test(rating-service): add Firebase UID metadata to e2e tests

--- services/rating-service/tests/e2e/helpers_test.go
+++ services/rating-service/tests/e2e/helpers_test.go
@@ -3,7 +3,15 @@ package e2e
 import (
 	"context"
 	"testing"
+
+	"google.golang.org/grpc/metadata"
 )
+
+// ctxWithUID retourne un contexte avec le Firebase UID dans les metadata gRPC
+func ctxWithUID(uid string) context.Context {
+	md := metadata.Pairs("x-firebase-uid", uid)
+	return metadata.NewOutgoingContext(context.Background(), md)
+}
 
 // cleanupRatingsTable vide la table ratings (TRUNCATE)
 func cleanupRatingsTable(t *testing.T, ctx context.Context) {
